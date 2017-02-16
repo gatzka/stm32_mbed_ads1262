79947d6f003e3742a5273e8094d690fa1c779612
Stephan Gatzka <stephan.gatzka@hbm.com>
2017-02-16 14:46:26 +0100

    Re-enable DMA transfer in ISR.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ static DMA_Stream_TypeDef *dma2_stream0 = DMA2_Stream0;
 
 static Serial pc(USBTX, USBRX);
 
+static uint32_t from_value = 0x01010101;
+static uint32_t to_value = 0x0;
+
 static void disable_dma(void)
 {
 	dma2_stream0->CR &= ~DMA_SxCR_EN;
@@ -22,7 +25,7 @@ static void enable_dma(void)
 
 static void setup_transfer(void *to, void *from, uint32_t num)
 {
-	dma2_stream0->CR = DMA_SxCR_DIR_1 | DMA_SxCR_TCIE | DMA_SxCR_TEIE | DMA_SxCR_DMEIE;
+	dma2_stream0->CR = DMA_SxCR_DIR_1 | DMA_SxCR_TCIE | DMA_SxCR_TEIE | DMA_SxCR_DMEIE | DMA_SxCR_PSIZE_1 | DMA_SxCR_MSIZE_1;
 	dma2_stream0->PAR = (uint32_t)from;
 	dma2_stream0->M0AR = (uint32_t)to;
 	dma2_stream0->NDTR = num;
@@ -30,38 +33,51 @@ static void setup_transfer(void *to, void *from, uint32_t num)
 
 static void show_dma_regs(void)
 {
-	pc.printf("DMA2 LISR: %x\n", dma2->LISR);
-	pc.printf("DMA2 HISR: %x\n", dma2->HISR);
-	pc.printf("DMA2 CR: %8x\n", dma2_stream0->CR);
-	pc.printf("DMA2 NDTR: %8x\n", dma2_stream0->NDTR);
-	pc.printf("DMA2 PAR: %8x\n", dma2_stream0->PAR);
-	pc.printf("DMA2 M0AR: %8x\n", dma2_stream0->M0AR);
+	pc.printf("DMA2 LISR: 0x%08x\n", dma2->LISR);
+	pc.printf("DMA2 HISR: 0x%08x\n", dma2->HISR);
+	pc.printf("DMA2 CR: 0x%08x\n", dma2_stream0->CR);
+	pc.printf("DMA2 NDTR: 0x%08x\n", dma2_stream0->NDTR);
+	pc.printf("DMA2 PAR: 0x%08x\n", dma2_stream0->PAR);
+	pc.printf("DMA2 M0AR: 0x%08x\n", dma2_stream0->M0AR);
+}
+
+static void dma_irq_handler(void)
+{
+	static const uint32_t errors = DMA_LISR_FEIF0 | DMA_LISR_DMEIF0 | DMA_LISR_TEIF0;
+	uint32_t lisr = dma2->LISR;
+
+	if ((lisr & errors) == errors) {
+		pc.printf("DMA transfer error: lisr: 0x%08x\n", lisr);
+	} else {
+		if ((lisr & DMA_LISR_TCIF0) == DMA_LISR_TCIF0) {
+			pc.printf("DMA transfer complete, from: %08x, to: %08x\n", from_value, to_value);
+			from_value++;
+			enable_dma();
+		} else {
+			pc.printf("DMA transfer incomplete, lisr: 0x%08x\n", lisr);
+		}
+	}
+
+	uint32_t lifcr_mask = DMA_LIFCR_CFEIF0 | DMA_LIFCR_CDMEIF0 | DMA_LIFCR_CTEIF0 | DMA_LIFCR_CHTIF0 | DMA_LIFCR_CTCIF0;
+	dma2->LIFCR |= lifcr_mask;
 }
 
 int main(void) {
-	uint32_t from = 0x1;
-	uint32_t to = 0x2;
 
 	RCC_TypeDef *rcc = RCC;
 	rcc->AHB1ENR |= RCC_AHB1ENR_DMA2EN;;
 
 	pc.printf("\n");
 	disable_dma();
-	setup_transfer(&to, &from, sizeof(from));
+	setup_transfer(&to_value, &from_value, 1);
 	show_dma_regs();
+
+	NVIC_SetVector(DMA2_Stream0_IRQn, (uint32_t)&dma_irq_handler);
+	NVIC_EnableIRQ(DMA2_Stream0_IRQn);
 
 	pc.printf("Enable DMA\n");
 	enable_dma();
 	wait(1.0);
-	pc.printf("Looking at vars: %x %x\n", from, to);
-	show_dma_regs();
-
-	from = 5;
-	to = 6;
-	pc.printf("Enable DMA\n");
-	enable_dma();
-	wait(1.0);
-	pc.printf("Looking at vars: %x %x\n", from, to);
 	show_dma_regs();
 
 	while (true) {
